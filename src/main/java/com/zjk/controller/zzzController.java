a387b231b4236485831eb55e6f53a5778dc87414
一只大大鱼 <2417924164@qq.com>
2022-09-15 16:43:36 +0800

    second commit

--- src/main/java/com/zjk/controller/zzzController.java
+++ src/main/java/com/zjk/controller/zzzController.java
@@ -3,5 +3,6 @@ package com.zjk.controller;
 public class zzzController {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
     }
 }
